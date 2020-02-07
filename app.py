6ba86e1da9370e63c455014a5616f5030ce3784e
Bikal <basnet.beekal@gmail.com>
2020-02-06 22:00:05 -0500

    ADD: app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+
+app = Flask(__name__, instance_relative_config=True)
+app.secret_key = ""
+CORS(app)
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    print('Hello from the dummy app')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
+
